a19759f03d6839a53cf721bf40ea0a7b121a5119
jo-va <vandamme_jonathan@hotmail.com>
2018-01-02 10:06:12 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,18 +26,17 @@ socket.on('newLocationMessage', function(message) {
 	$('#messages').append($li);
 });
 
-var $messageInput = $('[name=message]');
-
 $('#message-form').on('submit', function(e) {
 	e.preventDefault();
+
+	var $messageInput = $('[name=message]');
 
 	socket.emit('createMessage', {
 		from: 'User',
 		text: $messageInput.val()
 	}, function() {
-
+		$messageInput.val('');
 	});
-	$messageInput.val('');
 });
 
 var $locationButton = $('#send-location');
@@ -45,12 +44,17 @@ $locationButton.on('click', function() {
 	if (!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	}
+
+	$locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function(position) {
+		$locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			lat: position.coords.latitude,
 			lng: position.coords.longitude
 		});
 	}, function(err) {
+		$locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location', err);
 	});
 });
